a36c9ae7b3e0707b4c60d500f65199d7bdc96dc6
diana <diana.mirrezaee@gmail.com>
2024-12-27 15:41:41 +0330

    add animate

--- AdminBookShop/wwwroot/js/scripts.js
+++ AdminBookShop/wwwroot/js/scripts.js
@@ -6,3 +6,14 @@
         desktopNav.classList.toggle('active');  // Toggle the 'active' class to show/hide the menu
     });
 });
+
+
+// AOS initialization script
+AOS.init({
+    offset: 120, // Start animation 120px before element enters the viewport
+    duration: 1000, // Animation duration in ms
+    easing: 'ease', // Easing function
+    delay: 100, // Delay before animation starts
+    once: true // Trigger animation only once
+});
+
